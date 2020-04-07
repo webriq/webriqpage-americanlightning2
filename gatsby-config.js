5023af18df2d87272cb70906ef701b0fbfb686b5
Dorell James Galang <galangdj@gmail.com>
2020-04-07 09:53:04 +0800

    Temporarily disable gatsby-plugin-manifest for successful build

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,18 +112,18 @@ module.exports = {
         token: `skdYTE9CTZKbrvYwZFtk8gbTZvfyTe1Pl0KzEnvpP9wWEprkbjsVDwF4aycWaA1TlgG1vXSVMdvUvSBPIKiYpShq51qePwOa0Rh6Vpiu0wSQNzDsKdW5k7uTVT9ZXEW0oeR3EdREjrNGsxymPtat2CRpvFO6asKz5bfgjt0ShNMBAzNedz4G`,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Sanity CMS Template`,
-        short_name: `sanitytemplate`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `Sanity CMS Template`,
+    //     short_name: `sanitytemplate`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
