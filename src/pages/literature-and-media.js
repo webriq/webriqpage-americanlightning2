8ab23414d502497210d6205f5fcfcf385232ffd2
tbalisacan <tessa.balisacan@gmail.com>
2020-04-08 17:26:26 +0800

    catalogs

--- src/pages/literature-and-media.js
+++ src/pages/literature-and-media.js
@@ -7,11 +7,78 @@ import { LazyLoadComponent } from "react-lazy-load-image-component"
 import ModalVideo from "../components/literature/ModalVideo"
 import ProductCatalog from "../components/literature/productCatalog"
 
+import AmLight2020 from "../images/catalog_thumbnails/2020 al cover_-u193120-fr.png"
+import AmLight2019 from "../images/catalog_thumbnails/american lighting 2019 cover-u184642-fr.png"
+import AmLightProd2019 from "../images/catalog_thumbnails/american lighting 2019 product guide cover_page_001.jpg"
+import Seasonal2019 from "../images/catalog_thumbnails/_thumb_al_seasonalcat_2019_cover.jpg"
+import TruluxLightSys from "../images/catalog_thumbnails/_thumb_catalog_trulux.png"
+import LuneFX from "../images/catalog_thumbnails/_thumb_lumefx-brochure.jpg"
+import FrameWRX from "../images/catalog_thumbnails/_thumb_framewrx-brochure.jpg"
+import TruluxCC from "../images/catalog_thumbnails/_thumb_brochure_truluxcc.jpg"
+import TruluxSingleColor from "../images/catalog_thumbnails/_thumb_form-truluxcc-singlecolor.jpg"
+import TruluxCCTunable from "../images/catalog_thumbnails/_thumb_form-truluxcc-tunable.jpg"
+import TruluxCCRGB from "../images/catalog_thumbnails/_thumb_form-truluxcc-rgb.jpg"
+
 const catalogs = [
 	{
-		thumbnail: "/assets/catalogs/thumbs/2020 al cover_-u193120-fr.png",
 		title: "American Lighting 2020 Catalog",
-		url: "/",
+		thumbnail: AmLight2020,
+		url:
+			"http://www.americanlighting.com/assets/_catalog_american-lighting-2020_rev2005.pdf",
+	},
+	{
+		title: "American Lighting 2019 Catalog",
+		thumbnail: AmLight2019,
+		url:
+			"http://www.americanlighting.com/assets/_catalog_american-lighting-2019-catalog_web.pdf",
+	},
+	{
+		title: "American Lighting Product Guide 2019",
+		thumbnail: AmLightProd2019,
+		url:
+			"http://americanlighting.com/catalogs/American-Lighting-2019-Product-Guide",
+	},
+	{
+		title: "Seasonal 2019 Catalog",
+		thumbnail: Seasonal2019,
+		url: "http://americanlighting.com/catalogs/AL_SeasonalCat_2019",
+	},
+	{
+		title: "Trulux Lighting Systems",
+		thumbnail: TruluxLightSys,
+		url: "http://www.americanlighting.com/assets/_catalog_trulux2016.pdf",
+	},
+	{
+		title: "LuneFX Tunable Lighting",
+		thumbnail: LuneFX,
+		url: "http://americanlighting.com/catalogs/_LumeFX_rev1753",
+	},
+	{
+		title: "FrameWRX Exterior",
+		thumbnail: FrameWRX,
+		url: "http://americanlighting.com/catalogs/_FrameWRX_rev1753",
+	},
+	{
+		title: "Trulux Custom Cut Brochure",
+		thumbnail: TruluxCC,
+		url: "http://www.americanlighting.com/assets/_brochure_truluxcustomcut.pdf",
+	},
+	{
+		title: "Trulux Custom Cut Single Color Order Form",
+		thumbnail: TruluxSingleColor,
+		url:
+			"http://www.americanlighting.com/assets/_form_truluxcc-singlecolor_int.pdf",
+	},
+	{
+		title: "Trulux Custom Cut Tunable CCT Order Form",
+		thumbnail: TruluxCCTunable,
+		url:
+			"http://www.americanlighting.com/assets/_form_truluxcc-tunable_int.pdf",
+	},
+	{
+		title: "Trulux Custom Cut RGB/RGB+WW Order Form",
+		thumbnail: TruluxCCRGB,
+		url: "http://www.americanlighting.com/assets/_form_truluxcc-rgb_int.pdf",
 	},
 ]
 
