d244885525066c7041be6b0a3795d6d1405723e3
Dorell James Galang <galangdj@gmail.com>
2020-04-10 16:45:16 +0800

    Enable sending postMessage when embedded in iframe

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header/header"
@@ -10,7 +10,46 @@ import "font-awesome/css/font-awesome.min.css"
 import "./font-face.css"
 
 const Layout = ({ children, type }) => {
-  // console.log(type)
+  function bindEvent(element, eventName, eventHandler) {
+    if (element.addEventListener) {
+      element.addEventListener(eventName, eventHandler, false)
+    } else if (element.attachEvent) {
+      element.attachEvent("on" + eventName, eventHandler)
+    }
+  }
+
+  // Send a message to the parent
+  const sendMessage = function (msg) {
+    // Make sure you are sending a string, and to stringify JSON
+    window.parent.postMessage(msg, "*")
+  }
+
+  useEffect(() => {
+    console.log("sendBackMessage initiated!")
+
+    bindEvent(window, "message", function (e) {
+      console.log("message received at iFrame!")
+      console.log("e", e)
+    })
+
+    let sendBackCount = 1
+    const sendBackMessage = setInterval(function () {
+      if (sendBackCount > 100) {
+        clearInterval(sendBackMessage)
+      }
+
+      console.log("Sending message: ALIVE - Count: " + sendBackCount)
+      sendMessage("ALIVE")
+      sendBackCount = sendBackCount + 1
+    }, 3000)
+
+    // Unload
+    return () => {
+      console.log("sendBackMessage unloaded!")
+      clearInterval(sendBackMessage)
+    }
+  }, [])
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
